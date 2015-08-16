7583f8d29877dff2dbd0817eafb1c02df9689067
bennacer860 <bennacer860@gmail.com>
2015-08-16 15:49:57 -0400

    refactor world

--- lib/world.rb
+++ lib/world.rb
@@ -33,33 +33,37 @@ class World
     @grid.seed_a_cell(0,1)
     @grid.seed_a_cell(1,1)
   end
+        
+  def execute_moves(moves)
+    moves.each{ |order|
+      @board[order[:row]][order[:column]].alive = order[:alive]
+    }
+  end
 
   def tick(generations)
     0.upto(generations) do
+      moves = []
       system("clear")
       @grid.show_board
       sleep(1)
-      future_orders = []
-
+ 
       @board.each_index do |row|
         @board[row].each_index do |column|
           cell = @board[row][column]
           number_of_neighbors =  @grid.number_of_alive_cells_around(row,column) 
           # Any live cell with fewer than two live neighbours dies, as if by needs caused by underpopulation.
-          future_orders << {row: row, column: column, alive: false} if cell.alive && number_of_neighbors < 2
+          moves << {row: row, column: column, alive: false} if cell.alive && number_of_neighbors < 2
           # Any live cell with more than three live neighbours dies, as if by overcrowding.
-          future_orders << {row: row, column: column, alive: false} if cell.alive && number_of_neighbors > 3
+          moves << {row: row, column: column, alive: false} if cell.alive && number_of_neighbors > 3
           # Any live cell with two or three live neighbours lives, unchanged, to the next generation.
-          future_orders << {row: row, column: column, alive: true} if cell.alive && (number_of_neighbors == 2 || number_of_neighbors == 3)
+          moves << {row: row, column: column, alive: true} if cell.alive && (number_of_neighbors == 2 || number_of_neighbors == 3)
           # Any dead cell with exactly three live neighbours cells will come to life.
-          future_orders << {row: row, column: column, alive: true}if !cell.alive && number_of_neighbors == 3
+          moves << {row: row, column: column, alive: true}if !cell.alive && number_of_neighbors == 3
         end
       end
 
       # the trick here is to execute all the move at the end of the tick
-      future_orders.each{ |order|
-        @board[order[:row]][order[:column]].alive = order[:alive]
-      }
+      execute_moves(moves)
     end
   end
 
